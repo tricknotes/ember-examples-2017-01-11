2ee48db71b5a84037745bb44a07329b4ee635e27
Ryunosuke Sato <tricknotes.rs@gmail.com>
2017-01-10 16:04:35 +0900

    Show starred repositories at user template

--- github-star-viewer/app/models/user.js
+++ github-star-viewer/app/models/user.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   login: DS.attr(),
-  avatar_url: DS.attr()
+  avatar_url: DS.attr(),
+
+  starred: DS.hasMany('repository')
 });
--- github-star-viewer/app/serializers/user.js
+++ github-star-viewer/app/serializers/user.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.RESTSerializer.extend({
   primaryKey: 'login',
 
-  normalizeFindRecordResponse(store, primaryModelClass, payload, id, requestType) {
+  normalizeSingleResponse(store, primaryModelClass, payload, id, requestType) {
     payload.links = {
       starred: `${payload.url}/starred`
     };
