bb36fe52a92642fb81db6ebc026c1c573bd20a60
Ryunosuke Sato <tricknotes.rs@gmail.com>
2017-01-10 16:11:28 +0900

    Show recent searched users

--- github-star-viewer/app/routes/index.js
+++ github-star-viewer/app/routes/index.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.peekAll('user');
+  },
+
   actions: {
     addUser(login) {
       const user = this.store.findRecord('user', login);
